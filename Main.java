ccb07a3a54f9c87210bcc7b4857211965330e59e
Simon Mossmyr <mossmyr@live.com>
2017-06-07 15:01:41 +0200

    added test file, changed gradients from edge centers to unit sphere

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main{
+	public static void main(String[] args) {
+		Noise noise = new Noise(2);
+	}
+}
--- Noise.java
+++ Noise.java
@@ -1,38 +1,145 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Collections;
+
+/**
+ * Perlin's unmodified 1985 algorithm to arbitrary dimensions.
+ * The complexity of the algorithm is O(n*2^n), so for higher dimensions this program is incredibly slow.
+ *
+ * @author Simon Mossmyr
+ */
 public class Noise {
 
+	// Constant variables.
+	private final static 	int 	DEFAULT_GRADIENT_LIST_SIZE 				= 256;	
+	private final static 	int 	DEFAULT_PERMUTATION_LIST_SIZE 			= 256;	
+	private final 			String 	INVALID_POINT_DIMENSION_ERROR_MESSAGE 	= "Input point and Noise object are of different dimensions.";
+	private final static	int 	DEFAULT_DIMENSION						= 3;
+
+	// Global variables.
+	private ArrayList<ArrayList<Double>>	gradientList;
+	private ArrayList<Integer> 				permutationList;
+	private int 							dimension;
+
+	/**
+	 * Default constructor.
+	 */
+	public Noise() {
+		this(DEFAULT_DIMENSION);
+	}
+
+	/**
+	 * Constructor with variable dimension.
+	 */
 	public Noise(int dimension) {
+		this(dimension, DEFAULT_GRADIENT_LIST_SIZE, DEFAULT_PERMUTATION_LIST_SIZE);
+	}
+
+	/**
+	 * Super constructor with variable dimension, gradient- and permutation list size.
+	 */
+	public Noise(int dimension, int gradientListSize, int permutationListSize) {
+		this.dimension = dimension;
+		gradientList = generateGradientList(dimension, gradientListSize);
+		permutationList = generatePermutationList(permutationListSize);
+	}
+
+	/**
+	 * Generate a list of normal n-vectors, uniformly distributed on the surface of a n-hypersphere.
+	 */
+	private ArrayList<ArrayList<Double>> generateGradientList(int dimension, int size) {
+
+		ArrayList<ArrayList<Double>> gradientList = new ArrayList<>();
+		Random random = new Random();
+
+		for (int i = 0; i < size; i++) {
+			ArrayList<Double> gradient = new ArrayList<>();
+			double squareSum = 0.;
+
+			// Generate a n-vector from Gaussian random variables
+			for (int j = 0; j < dimension; j++) {
+				double element = random.nextGaussian();
+
+				gradient.add(element);
+				squareSum += element*element;
+			}
+
+			// Calculate 1/sqrt(x1^2 + x2^2 + x3^2 + ... + xn^2) as a factor
+			double factor = 1. / Math.sqrt(squareSum);
+
+			// Normalize the n-vector by multiplying every element of it with the above factor.
+			// Because every element is of Gaussian distribution, the normalized n-vector will be uniformly distributed on the surface of a n-hypersphere.
+			for (int j = 0; j < dimension; j++) {
+				gradient.set(j, gradient.get(j) * factor);
+			}
+
+			gradientList.add(gradient);
+		}
+		
+		return gradientList;
+	}
+
+	/**
+	 * Generate a permutation of the list of indices in the gradient list.
+	 */
+	private ArrayList<Integer> generatePermutationList(int size) {
+		ArrayList<Integer> permutationList = new ArrayList<>();
+
+		for (int i = 0; i < size; i++) {
+			permutationList.add(i % gradientList.size());
+		}
+
+		Collections.shuffle(permutationList);
+
+		return permutationList;
+	}
+
+	/**
+	 * Evaluate the noise value at a point in n-dimensional space.
+	 * The dimension of the input point must equal the dimension of the Noise object.
+	 */
+	private double evaluate(ArrayList<Double> point) throws Exception {
+		if (point.size() != dimension) {
+			throw new Exception(INVALID_POINT_DIMENSION_ERROR_MESSAGE);
+		}
+
+		// Step 1: Floor all elements of the input point
+		ArrayList<Integer> flooredPoint = new ArrayList<>();
+		for (int i = 0; i < dimension; i++) {
+			flooredPoint.add((int)Math.floor(point.get(i)));
+		}
+
+		// Step 2: Find the relative coordinates of the point inside the closest n-hypercube of the n-hypercube point lattice.
+		ArrayList<Double> relativePoint = new ArrayList<>();
+		for (int i = 0; i < dimension; i++) {
+			relativePoint.add(point.get(i) - flooredPoint.get(i));
+		}
+
+		// Step 3: 
+
+
+
+		return 0.;
 
 	}
 
 	/**
-	 * Generate a list of gradients as the set of vectors from the center of a n-dimensional hypercube to the mid-point of its edges.
-	 * To increase performance, the list is padded with 
-	 *
-	 * Complexity: O(n*2^n)
+	 * Returns the gradient assigned to the input point on the n-hypercube point lattice.
 	 */
-	private List<List<Integer>> generateGradients(int dimension) {
+	private ArrayList<Double> getGradient(ArrayList<Integer> point) {
 
-		// n*2^(n-1) number of edges
 
-		// 2D: 	( 1, 0) (0,  1) 
-		// 		(-1, 0) (0, -1)
-		//
-		// 3D: 	( 1,  1, 0) ( 1, 0,  1) (0,  1,  1)
-		//		( 1, -1, 0) ( 1, 0, -1) (0,  1, -1)
-		//		(-1,  1, 0) (-1, 0,  1) (0, -1,  1)
-		//		(-1, -1, 0) (-1, 0, -1) (0, -1, -1)
-		//
-		// 4D:	( 1,  1,  1, 0) ( 1,  1, 0,  1) ( 1, 0,  1,  1) (0,  1,  1,  1)
-		// 		( 1,  1, -1, 0) ( 1,  1, 0, -1) ( 1, 0,  1, -1) (0,  1,  1, -1)
-		// 		( 1, -1,  1, 0) ( 1, -1, 0,  1) ( 1, 0, -1,  1) (0,  1, -1,  1)
-		// 		( 1, -1, -1, 0) ( 1, -1, 0, -1) ( 1, 0, -1, -1) (0,  1, -1, -1)
-		//  	(-1,  1,  1, 0) (-1,  1, 0,  1) (-1, 0,  1,  1) (0, -1,  1,  1)
-		// 		(-1,  1, -1, 0) (-1,  1, 0, -1) (-1, 0,  1, -1) (0, -1,  1, -1)
-		// 		(-1, -1,  1, 0) (-1, -1, 0,  1) (-1, 0, -1,  1) (0, -1, -1,  1)
-		// 		(-1, -1, -1, 0) (-1, -1, 0, -1) (-1, 0, -1, -1) (0, -1, -1, -1)
 
-		
+		return new ArrayList<Double>();
+	}
 
+	private int hash(ArrayList<Integer> point, int index) {
+
+		if (index == dimension - 1) {
+			return point.get(index);
+		} else {
+			return point.get(index) + permutationList.get(hash(index+1) % permutationList.size());
+		}
 
 	}
 
