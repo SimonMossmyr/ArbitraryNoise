fd74e5581f20acdc7bca6eb97bf3e9eb3f88aa14
Simon Mossmyr <mossmyr@live.com>
2017-06-08 03:01:18 +0200

    finised evalute() method, only testing left

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Main{
 	public static void main(String[] args) {
+
 		Noise noise = new Noise(2);
+
+		for (double i = 0.1; i < 1.; i += 0.1) {
+			for (double j = 0.1; j < 1.; j += 0.1) {
+				ArrayList<Double> point = new ArrayList<>();
+				point.add(i);
+				point.add(j);
+				System.out.printf("%f.5 ", noise.evaluate(point));
+			}
+			System.out.println();
+
+		}
+
+
+
 	}
 }
--- Noise.java
+++ Noise.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Collections;
+import java.util.BitSet;
 
 /**
  * Perlin's unmodified 1985 algorithm to arbitrary dimensions.
- * The complexity of the algorithm is O(n*2^n), so for higher dimensions this program is incredibly slow.
+ * The complexity of the algorithm is O(2^n), so for higher dimensions this program is incredibly slow.
  *
  * @author Simon Mossmyr
  */
@@ -13,8 +14,9 @@ public class Noise {
 	// Constant variables.
 	private final static 	int 	DEFAULT_GRADIENT_LIST_SIZE 				= 256;	
 	private final static 	int 	DEFAULT_PERMUTATION_LIST_SIZE 			= 256;	
-	private final 			String 	INVALID_POINT_DIMENSION_ERROR_MESSAGE 	= "Input point and Noise object are of different dimensions.";
 	private final static	int 	DEFAULT_DIMENSION						= 3;
+	private final 			String 	INVALID_POINT_DIMENSION_ERROR_MESSAGE 	= "Input point and Noise object are of different dimensions.";
+	private final 			String 	INVALID_DIMENSION_ERROR_MESSAGE 		= "Dimension must be greater than 0.";
 
 	// Global variables.
 	private ArrayList<ArrayList<Double>>	gradientList;
@@ -38,7 +40,11 @@ public class Noise {
 	/**
 	 * Super constructor with variable dimension, gradient- and permutation list size.
 	 */
-	public Noise(int dimension, int gradientListSize, int permutationListSize) {
+	public Noise(int dimension, int gradientListSize, int permutationListSize) throws RuntimeException {
+		if (dimension < 1) {
+			throw new RuntimeException(INVALID_DIMENSION_ERROR_MESSAGE);
+		}
+
 		this.dimension = dimension;
 		gradientList = generateGradientList(dimension, gradientListSize);
 		permutationList = generatePermutationList(permutationListSize);
@@ -98,10 +104,14 @@ public class Noise {
 	 * Evaluate the noise value at a point in n-dimensional space.
 	 * The dimension of the input point must equal the dimension of the Noise object.
 	 */
-	private double evaluate(ArrayList<Double> point) throws Exception {
+	public double evaluate(ArrayList<Double> point) throws RuntimeException {
+		// Input validation
 		if (point.size() != dimension) {
-			throw new Exception(INVALID_POINT_DIMENSION_ERROR_MESSAGE);
+			throw new RuntimeException(INVALID_POINT_DIMENSION_ERROR_MESSAGE);
 		}
+
+		// Variables used throughout the method
+		int derp = (int)Math.pow(2,dimension);
 
 		// Step 1: Floor all elements of the input point
 		ArrayList<Integer> flooredPoint = new ArrayList<>();
@@ -109,17 +119,58 @@ public class Noise {
 			flooredPoint.add((int)Math.floor(point.get(i)));
 		}
 
-		// Step 2: Find the relative coordinates of the point inside the closest n-hypercube of the n-hypercube point lattice.
+		// Step 2: Find the relative coordinates of the point inside the surrounding n-hypercube
 		ArrayList<Double> relativePoint = new ArrayList<>();
 		for (int i = 0; i < dimension; i++) {
 			relativePoint.add(point.get(i) - flooredPoint.get(i));
 		}
 
-		// Step 3: 
+		// Step 3: Find the gradients assigned to the vertices of the surrounding n-hypercube
+		ArrayList<ArrayList<Double>> gradients = new ArrayList<>();
+		for(int i = 0; i < derp; i++) {
+			ArrayList<Integer> vertex = new ArrayList<>();
+			for (int j = 0; j < dimension; j++) {
+				vertex.add(flooredPoint.get(j) + (((i >> j) & 1) == 1 ? 1 : 0));
+			}
+			gradients.add(getGradient(vertex));
+		}
 
+		// Step 4: Find the distance vectors
+		ArrayList<ArrayList<Double>> distanceVectors = new ArrayList<>();
+		for (int i = 0; i < derp; i++) {
+			ArrayList<Double> distanceVector = new ArrayList<>();
+			for (int j = 0; j < dimension; j++) {
+				distanceVector.add(point.get(j) - (((i >> j) & 1) == 1 ? 1 : 0));
+			}
+			distanceVectors.add(distanceVector);
+		}
 
+		// Step 5: Find the noise contributions
+		ArrayList<Double> noiseContributions = new ArrayList<>();
+		for (int i = 0; i < derp; i++) {
+			noiseContributions.add(dot(gradients.get(i), distanceVectors.get(i)));
+		}
 
-		return 0.;
+		// Step 6: Blend the noise contributions by iterating over every dimension
+		ArrayList<ArrayList<Double>> merp = new ArrayList<>();
+		ArrayList<Double> gerp = new ArrayList<>();
+		for (int j = 0; j < noiseContributions.size(); j += 2) {
+			double blend = blend(relativePoint.get(0));
+			gerp.add(noiseContributions.get(j) * (1 - blend) + noiseContributions.get(j+1) * blend);
+		}
+		merp.add(gerp);
+
+		for (int i = 1; i < dimension; i++) {
+			ArrayList<Double> berp = new ArrayList<>();
+			for (int j = 0; j < merp.get(i-1).size(); j += 2) {
+				double blend = blend(relativePoint.get(i));
+				berp.add(merp.get(i-1).get(j) * (1 - blend) + merp.get(i-1).get(j+1) * blend);
+			}
+			merp.add(berp);
+		}
+
+		// Return final noise value
+		return merp.get(dimension-1).get(0);
 
 	}
 
@@ -127,20 +178,37 @@ public class Noise {
 	 * Returns the gradient assigned to the input point on the n-hypercube point lattice.
 	 */
 	private ArrayList<Double> getGradient(ArrayList<Integer> point) {
-
-
-
-		return new ArrayList<Double>();
+		return gradientList.get(hash(point, 0));
 	}
 
+	/**
+	 * Recursive hash function used by getGradient().
+	 */
 	private int hash(ArrayList<Integer> point, int index) {
-
 		if (index == dimension - 1) {
-			return point.get(index);
+			return permutationList.get(point.get(index) % permutationList.size());
 		} else {
-			return point.get(index) + permutationList.get(hash(index+1) % permutationList.size());
+			return permutationList.get((point.get(index) + hash(point, index+1)) % permutationList.size());
+		}
+	}
+
+	/**
+	 * Dot (scalar) product of two ArrayList objects.
+	 */
+	private double dot(ArrayList<Double> a, ArrayList<Double> b) {
+		double sum = 0;
+
+		for (int i = 0; i < a.size(); i++) {
+			sum += a.get(i) * b.get(i);
 		}
 
+		return sum;
 	}
 
+	/**
+	 * Blending function.
+	 */
+	private double blend(double t) {
+		return t * t * t * (3 * t * (2 * t - 5) + 10); // 6t^5 - 15t^4 + 10t^3
+	}
 }
